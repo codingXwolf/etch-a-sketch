f3d09172fe2b64ebdb71c18e360da95bd75aa1ea
Jaime Ortiz <jxme88@gmail.com>
2020-09-28 21:26:46 -0700

    Removed table and tr. Attempting to create div with for loops

--- script.js
+++ script.js
@@ -1,41 +1,29 @@
-window.onload = function () {
-  const container = document.querySelector(".flex-container");
+// flex container
+let mainContainer = document.querySelector('.flex-container');
+mainContainer.textContent = 'Main Container';
 
-  // create a table
-  const table = document.createElement("TABLE");
+//div 
+let singleDiv = document.createElement('div');
+singleDiv.textContent = 'div';
 
-  const tableBody = document.createElement("tbody");
+// Grid verticle
+for (let i = 0; i < 32 + 1; i++) {
+    let tile = document.createElement('div');
+    tile.classList.add('tile');
+    tile.textContent = i;
+    tile.setAttribute('style', 'flex-direction: row; background: dodgerblue; height: 16px; width: 16px;')
+    mainContainer.appendChild(tile);
 
-  table.appendChild(tableBody);
-  
+}
 
-  for (let i = 0; i < 16; i++) {
-    //create a table row
-    const tableRow = document.createElement("tr");
-    tableRow.className = "cell";
-    tableBody.appendChild(tableRow);
+// Grid horizontal
+for (let k = 0; k < 16; k++) {
+    let breakElement = document.createElement('br');
+    let tile = document.createElement('div');
+    tile.classList.add('tile');
+    tile.textContent = k;
+    tile.setAttribute('style', 'flex-direction:column; background: green; height: 16px; width: 16px;')
+    mainContainer.appendChild(tile);
+    mainContainer.appendChild(breakElement)
 
-    for (let j = 0; j < 16; j++) {
-      //create a table
-      const tableCell = document.createElement("td");
-      tableRow.className = "cell";
-      tableRow.appendChild(tableCell);
-    }
-  }
-    container.appendChild(table);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-};
+}
