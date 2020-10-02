67933630604d5cf8b8b5915590a9a4a7d5313a73
Jaime Ortiz <jxme88@gmail.com>
2020-10-02 10:23:33 -0700

    Working on changing grid color using input color. When changing color it completely changes entire grid to selected color.

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 // flex container
-let mainContainer = document.querySelector('.main-container');
+let mainContainer = document.querySelector('#main-container');
+mainContainer.setAttribute('style', '')
+
+// Default color and color changes
+let defaultColor = '#000000';
+let colorChanges;
+
+//Change Color Button
+let inputColorChange = document.getElementById('inputColorChange');
 
 
 //Create Grid
@@ -9,13 +17,20 @@ function createGrid(rows, cols) {
 
     for (i = 0; i < (rows * cols); i++) {
         let newDivs = document.createElement("div");
-        newDivs.addEventListener("mousemove", colorChange);
+        newDivs.addEventListener("mousemove", (e) => {
+            e.target.style.backgroundColor = defaultColor;
+        });
         mainContainer.appendChild(newDivs).className = "grid-object";
     };
 };
 
 createGrid(16, 16);
 
-function colorChange(e) {
-    e.target.style.backgroundColor = "green";
+function updateFirst (e) {
+    let divs = document.querySelectorAll('.grid-object');
+    divs.forEach(div => {
+        div.style.backgroundColor = e.target.value;
+    })
 }
+
+inputColorChange.addEventListener('input', updateFirst, false);
